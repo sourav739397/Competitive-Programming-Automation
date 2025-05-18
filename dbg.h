62037d240605d2b1d5243d3718aca98c9000f19f
Sourav Mondal <sourav739397@gmail.com>
2025-05-18 10:03:30 +0600

    Add files via upload

---
+++ dbg.h
@@ -0,0 +1,11 @@
+// #include <bits/stdc++.h>
+// using namespace std;
+
+template<typename A, typename B> ostream& operator<<(ostream& os, const pair<A, B>& p) { return os << '(' << p.first << ", " << p.second << ')'; }
+
+template<typename T_container, typename T = typename enable_if<!is_same<T_container, string>::value, typename T_container::value_type>::type> ostream& operator<<(ostream& os, const T_container& v) { os << '{'; string sep; for (const T& x : v) os << sep << x, sep = ", "; return os << '}'; }
+
+void dbg_out() { cerr << endl; }
+template<typename Head, typename... Tail> void dbg_out(Head H, Tail... T) { cerr << ' ' << H; dbg_out(T...); }
+
+#define dbg(...) cerr << "\033[1;31m[" << #__VA_ARGS__ << "] =\033[0m", dbg_out(__VA_ARGS__)
