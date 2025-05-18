3955dd9f43e790bce93cf0d823db3d10020f0ea4
Sourav Mondal <sourav739397@gmail.com>
2025-05-18 09:42:11 +0600

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,12 +4,10 @@ set -e
 
 INSTALL_PATH="/usr/local/bin"
 SCRIPT_NAME="cprun"
-SCRIPT_URL="https://raw.githubusercontent.com/sourav739397/All-In-ONE-cp/blob/main/cprun.sh
+SCRIPT_URL="https://raw.githubusercontent.com/sourav739397/All-In-ONE-cp/blob/main/cprun.sh"
 
 echo "Starting CP script installer......."
-
-
-echo "Downloading script from GitHub........"
+# echo "Downloading script from GitHub........"
 curl -fsSL "$SCRIPT_URL" -o "$INSTALL_PATH/$SCRIPT_NAME"
 
 chmod +x "$INSTALL_PATH/$SCRIPT_NAME"
