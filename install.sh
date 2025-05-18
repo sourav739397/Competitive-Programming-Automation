606a763f46e0338f4623fa9d823a8ee55845f13b
Sourav Mondal <sourav739397@gmail.com>
2025-05-18 10:06:13 +0600

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,12 +4,16 @@ set -e
 
 INSTALL_PATH="/usr/local/bin"
 SCRIPT_NAME="cprun"
-SCRIPT_URL="https://raw.githubusercontent.com/sourav739397/All-In-ONE-cp/blob/main/cprun.sh"
+# Updated GitHub raw URL to point to the actual raw content of cprun.sh
+SCRIPT_URL="https://raw.githubusercontent.com/sourav739397/Competitive-Programming-Automation/main/cprun.sh"
 
-echo "Starting CP script installer......."
-# echo "Downloading script from GitHub........"
+echo "Starting CP script installer..."
+
+# Download the script from GitHub
+echo "Downloading script from GitHub..."
 curl -fsSL "$SCRIPT_URL" -o "$INSTALL_PATH/$SCRIPT_NAME"
 
+# Make it executable
 chmod +x "$INSTALL_PATH/$SCRIPT_NAME"
 
 echo "Installed: You can now use the script by typing: cprun"
