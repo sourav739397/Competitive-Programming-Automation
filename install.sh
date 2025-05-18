39d5868e07a7297e3bb08ca5693944d0a3362328
Sourav Mondal <sourav739397@gmail.com>
2025-05-18 09:37:08 +0600

    Update install.sh

--- install.sh
+++ install.sh
@@ -9,10 +9,10 @@ SCRIPT_URL="https://raw.githubusercontent.com/sourav739397/All-In-ONE-cp/blob/ma
 echo "Starting CP script installer......."
 
 # Check for root
-if [ "$EUID" -ne 0 ]; then
-  echo "⚠️  Please run as root: sudo sh -c \"\$(curl -fsSL https://...)\""
-  exit 1
-fi
+# if [ "$EUID" -ne 0 ]; then
+#   echo "⚠️  Please run as root: sudo sh -c \"\$(curl -fsSL https://...)\""
+#   exit 1
+# fi
 
 echo "Downloading script from GitHub........"
 curl -fsSL "$SCRIPT_URL" -o "$INSTALL_PATH/$SCRIPT_NAME"
